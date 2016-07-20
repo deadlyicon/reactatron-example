95f79cfced97d19fb2dc3d0e1e685596999f7f4a
Jared Grippe <jared@deadlyicon.com>
2016-07-20 13:30:48 -0700

    added serveral webpack options from http://jlongster.com/Backend-Apps-with-Webpack--Part-I

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,67 @@
+var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
 
 var root  = __dirname
 
-var browserJs = {
-  entry: root+'/browser',
+// this lists all node_modules as external so they dont
+// get packaged and the requires stay as is.
+// http://jlongster.com/Backend-Apps-with-Webpack--Part-I
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+
+var sourceMapSupperBannerPlugin = new webpack.BannerPlugin(
+  'require("source-map-support").install();',
+  { raw: true, entryOnly: false }
+)
+
+
+var serverJs = {
+  context: root+'/server',
+  entry: root+'/server/index.js',
+  target: 'node',
   output: {
-    path: 'dist/public',
+    path: root+'/dist',
+    filename: 'server.js',
+  },
+  devtool: 'sourcemap',
+  externals: nodeModules, // dont bundle any node_modules
+  plugins:  [
+    sourceMapSupperBannerPlugin,
+    new webpack.IgnorePlugin(/\.(css|less)$/)
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0'],
+          plugins: ['transform-runtime'],
+          cacheDirectory: true
+        }
+      }
+    ]
+  },
+}
+
+var browserJs = {
+  context: root+'/browser',
+  entry: root+'/browser/index.js',
+  output: {
+    path: root+'/dist/public',
     filename: 'browser.js',
     publicPath: '/'
   },
+  devtool: 'sourcemap',
   module: {
     loaders: [
       {
@@ -32,7 +84,7 @@ var clientCss = {
 };
 
 module.exports = [
-  // serverJs,
+  serverJs,
   browserJs,
   // browserCss,
 ]
